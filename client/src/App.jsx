f175457639b2087d7b7b01aca08f1f08cb87c0ad
Kavini Gayanthi <kavinigayanthi21@gmail.com>
2025-10-10 06:50:36 +0530

    new

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import AllRooms from "./pages/AllRooms";
+import RoomDetails from "./pages/RoomDetails";
 
 const App = () => {
 
@@ -13,10 +16,13 @@ const isOwnerPath = useLocation().pathname.includes("owner");
      <div className='min-h-[70vh]'>
       <Routes>
           <Route path='/' element= {<Home/>} />
+          <Route path='/rooms' element= {<AllRooms/>} />
+          <Route path='/rooms/:id' element= {<RoomDetails/>} />
 
       </Routes>
 
      </div>
+     <Footer />
     </div>
   )
 }
