89928967bf47de06e4543e0fa8928d6019508f80
Kavini Gayanthi <kavinigayanthi21@gmail.com>
2025-10-14 07:26:50 +0530

    new

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -77,10 +77,34 @@ const RoomDetails = () =>{
                        </div>
                        {/* checkIn checkOut Form */}
                        <form className='flex flex-col md:flex-row items-start md:items-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] p-6 rounded-xl mx-auto mt-16 max-w-6xl'>
-                            <div>
+                            <div className='flex flex-col flex-wrap md:flex-row items-start md:items-center gap-4 md:gap-10 text-gray-500'>
 
+                                <div className='flex flex-col'>
+                                    <label htmlFor="checkInDate" className='font-medium'>check-In</label>
+                                    <input type="data" id='checkInDate' placeholder='Check-In' className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required/>
+                                </div>
+
+
+                                <div className='flex flex-col'>
+                                    <label htmlFor="checkOutDate" className='font-medium'>check-Out</label>
+                                    <input type="data" id='checkOutDate' placeholder='Check-Out' className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required/>
+                                </div>
+
+                                <div className='flex flex-col'>
+                                    <label htmlFor="guests" className='font-medium'>guests</label>
+                                    <input type="number" id='guests' placeholder='0' className='max-w-20 rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required/>
+                                </div>
+                                
                             </div>
-                            <button type='submit' className='bg-primary hover:bg-primary-dull active:scale-95 tracking-all text-white rounded-md max-md:w-full max-md:mt-6 md:px-25 py-3 md:py-4 text-base cursor-pointer' >Book New</button>
+
+                            <button type='submit' className='bg-primary hover:bg-primary-dull active:scale-95 tracking-all text-white rounded-md max-md:w-full max-md:mt-6 md:px-25 py-3 md:py-4 text-base cursor-pointer' >
+                            Check Availability  </button>
+
+                            {/* common specification */}
+
+                            <div>
+                                
+                            </div>
                        </form>
 
         </div>
