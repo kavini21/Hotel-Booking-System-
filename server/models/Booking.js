728727feee44fc9ffa8061a3bcc6b15b3e69f01f
Kavini Gayanthi <kavinigayanthi21@gmail.com>
2025-10-26 00:06:48 +0530

    ADD BACKEND

---
+++ server/models/Booking.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+   user: {type: String, ref: "User", required: true},
+   room: {type: String, ref: "Room", required: true},
+    hotel: {type: String, ref: "Hotel", required: true},
+    checkInData: {type: Data, required: true},
+    checkOutData: {type: Data, required: true},
+    totalPrice: {type: Number, required: true},
+    guests: { type: Number, required: true},
+    status: {
+        type: String,
+        enum: ["pending", "confirmed", "cancelled"],
+        default: "pending",
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+        default: "pay At Hotel",
+    },
+    isPaid: {type: Boolean, default: false}
+    
+
+},{timestamps: true});
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+export default Booking;
